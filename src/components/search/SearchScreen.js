92396fc4b9646c60ad3c7b3ed6fee069fb44aa1e
AlexisNarvaez <alexisnarvaez97@hotmail.com>
2020-08-27 20:35:28 -0500

    Heroe no encontrado errores

--- src/components/search/SearchScreen.js
+++ src/components/search/SearchScreen.js
@@ -8,19 +8,19 @@ import { getHeroesByName } from "../../selectors/getHeroesByName";
 
 export const SearchScreen = ({ history }) => {
   const location = useLocation();
-//   console.log(location.search);
-  const { q = '' } = queryString.parse(location.search);
-//   console.log(q);
+  //   console.log(location.search);
+  const { q = "" } = queryString.parse(location.search);
+  //   console.log(q);
 
   const [formValues, handleInputChange, reset] = useForm({
-      searchText: q,
-    });
-    
-    const { searchText } = formValues;
+    searchText: q,
+  });
 
-    const heroesFiltered= useMemo(() => getHeroesByName(q), [ q ])
-    
-    // const heroesFiltered = getHeroesByName(searchText);
+  const { searchText } = formValues;
+
+  const heroesFiltered = useMemo(() => getHeroesByName(q), [q]);
+
+  // const heroesFiltered = getHeroesByName(searchText);
 
   const handleSearch = (e) => {
     e.preventDefault();
@@ -58,6 +58,10 @@ export const SearchScreen = ({ history }) => {
         <div className="col-7">
           <h4> Results </h4>
           <hr />
+
+          {q === "" && <div className="alert alert-info">Search a hero</div>}
+
+          { (q !== "" && heroesFiltered.length  === 0) && <div className="alert alert-danger"> No hay un heroe llamado ${ q } </div>}
 
           {heroesFiltered.map((hero) => (
             <HeroCard key={hero.id} {...hero} />
