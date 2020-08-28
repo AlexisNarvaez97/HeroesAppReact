2c10e69bd8ddba52877fbbb7cfe29739a1fe219a
AlexisNarvaez <alexisnarvaez97@hotmail.com>
2020-08-27 20:12:08 -0500

    Custom Hook Form

--- src/components/heroes/HeroesList.js
+++ src/components/heroes/HeroesList.js
@@ -1,18 +1,17 @@
-import React from 'react';
-import { getHeroesByPublisher } from '../../selectors/getHeroesByPublisher';
-import { HeroCard } from './HeroCard';
+import React, { useMemo } from "react";
+import { getHeroesByPublisher } from "../../selectors/getHeroesByPublisher";
+import { HeroCard } from "./HeroCard";
 
 export const HeroesList = ({ publisher }) => {
+  const heroes = useMemo(() => getHeroesByPublisher(publisher), [publisher]);
 
-    const heroes = getHeroesByPublisher( publisher );
+  // const heroes = getHeroesByPublisher( publisher );
 
-    return (
-        <div className="card-columns">
-
-            {heroes.map( hero => (
-                <HeroCard key={hero.id} {...hero}/>
-            ))}
-            
-        </div>
-    )
-}
+  return (
+    <div className="card-columns">
+      {heroes.map((hero) => (
+        <HeroCard key={hero.id} {...hero} />
+      ))}
+    </div>
+  );
+};
--- src/routers/DashboardRouter.js
+++ src/routers/DashboardRouter.js
@@ -6,6 +6,7 @@ import { Navbar } from "../components/ui/NavBar";
 import { MarvelScreen } from "../components/marvel/MarvelScreen";
 import { HeroesScreen } from "../components/heroes/HeroesScreen";
 import { DcScreen } from "../components/dc/DcScreen";
+import SearchScreen from "../components/search/SearchScreen";
 
 export const DashboardRouter = () => {
   return (
@@ -15,8 +16,9 @@ export const DashboardRouter = () => {
       <div className="container mt-3">
         <Switch>
           <Route exact path="/marvel" component={MarvelScreen} />
-          <Route exact path="/heroe/:heroeId" component={HeroesScreen} />
+          <Route exact path="/hero/:heroeId" component={HeroesScreen} />
           <Route exact path="/dc" component={DcScreen} />
+          <Route exact path="/search" component={ SearchScreen } />
           <Redirect to="/marvel"/>
         </Switch>
       </div>
